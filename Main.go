1bc18f0a65e6e2ed331a08219eb7b5370fc10402
Miguel_Lara <miguel.lara@espiralsistemas.com>
2024-02-04 05:40:48 -0600

    segundo comit cambio de prueba

--- Main.go
+++ Main.go
@@ -21,14 +21,14 @@ func main() {
 	fmt.Println(v_ip)
 	fmt.Println("2")
 
-	//file, err := os.Create("config.ini")
-	//if err != nil {
-	//	panic(err)
-	//}
+	file, err := os.Create("config.ini")
+	if err != nil {
+		panic(err)
+	}
 	//v_directorio := "c:/microsip datos"
 	//data := []byte(v_directorio)
 	//file.Write(data)
 	//file.WriteString("este es un string \n")
 	//file.WriteString("este es un string2")
-	//file.Close()
+	file.Close()
 }
